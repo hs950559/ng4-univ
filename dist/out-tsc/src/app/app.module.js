033e819aaefed6ec2d5151060ac07d88ee4ac252
hemansin <hemansin>
2017-06-09 01:06:46 +0530

    universal starter ready

---
+++ dist/out-tsc/src/app/app.module.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_1 = require("@angular/platform-browser");
+var core_1 = require("@angular/core");
+var app_routing_module_1 = require("./app-routing.module");
+var app_component_1 = require("./app.component");
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule.decorators = [
+    { type: core_1.NgModule, args: [{
+                declarations: [
+                    app_component_1.AppComponent
+                ],
+                imports: [
+                    platform_browser_1.BrowserModule.withServerTransition({ appId: 'ang4-seo-pre' }),
+                    app_routing_module_1.AppRoutingModule
+                ],
+                providers: [],
+                bootstrap: [app_component_1.AppComponent]
+            },] },
+];
+/** @nocollapse */
+AppModule.ctorParameters = function () { return []; };
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MyPage } from './app.po';
+
+describe('my App', () => {
+  let page: MyPage;
+
+  beforeEach(() => {
+    page = new MyPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    // expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
